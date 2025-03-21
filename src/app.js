7b8107771992d64886307c93d7cecde935f8f88e
alena.budnik <alena.budnik@opstalent.com>
2025-03-21 16:31:51 +0300

    feat: Set up CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import cors from 'cors'; 
 import uploadRoutes from './routes/uploadRoutes.js';
 
 const app = express();
+
+const corsOptions = {
+    origin: `${process.env.CLIENT_URL}`,
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type', 
+  };
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
